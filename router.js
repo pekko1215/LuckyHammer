b4d3ece4636bf3cd369b5a34ecc897f30cc85b85
pekko1215 <pekko1215@yahoo.co.jp>
2018-02-10 19:33:37 +0900

    plus_socket.io

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 module.exports = function(app,server,passport){
     app.get('/',function(req,res,next){
         if(req.user){
-            console.log(req.user);
             next();
             return;
         }
