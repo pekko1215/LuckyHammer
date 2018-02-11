e16bfa2ae89a0a475eafcd0f4e63241eddf52995
pekko1215 <pekko1215@yahoo.co.jp>
2018-02-11 20:12:14 +0900

    releasae

--- public/LuckyHammer.js
+++ public/LuckyHammer.js
@@ -6,27 +6,49 @@ $(()=>{
         "columns":[{
             "title":"ユーザ名",
             "orderable":"false",
-            "width":"30%"
+            "width":"10%",
+            "data":"in_reply_to_screen_name"
         },{
             "title":"ツイート内容",
             "orderable":"true",
-            "width":"50%"
+            "width":"70%",
+            "data":"text"
         },{
             "title":"時間",
             "orderable":"true",
-            "width":"10%"
+            "width":"10%",
+            "data":"created_at"
         },{
             "title":"画像URL",
-            "width":"10%"
+            "width":"10%",
+            "data":"entities"
         }],
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Japanese.json"
+        },
+        aaSorting:[[2,"asc"]],
+        fnCreatedRow:function(nRow,aData,iDataIndex){
+          var $nRow = $(nRow);
+          var $td = $nRow.find('td').eq(3);
+          $td.html('')
+          if(!aData.entities.media){return}
+          aData.entities.media.forEach((media)=>{
+              console.log(media)
+              var $img = $('<img>',{src:media.media_url_https,width:"32px"});
+              $td.append($img)
+          })
         }
     });
     var socket = io.connect('/');
+    window.dataTable = dataTable
     socket.on('tweet', function(data) {
-        console.log(data);
-        dataTable.api().rows.add(data)
+        data.created_at = (new Date(data.created_at)).toLocaleString()
+        dataTable.fnAddData(data);
+        //dataTable.fnSort([2,"desc"])
     });
     window.socket = socket
+
+    $('#start').click(()=>{
+      socket.emit('search',{target:$('#target').val()})
+    })
 })
--- socket.js
+++ socket.js
@@ -1,44 +1,47 @@
-/**
+/*
  * Created by pekko1215 on 2018/02/10.
  */
 const twitter = require('twitter')
 module.exports = function (app,http,socket,sessionStore) {
-    const io = socket.listen(http);
-    io.use(function(socket,next){
-        console.log('use')
-        app.session(socket.request,socket.request.res,next);
-    });
-    io.sockets.on('connection',function(socket){
-        console.log('connected')
-        var stream = null;
-        var tokens = require('./tokens').twitter;
-        var {session} = socket.request
-        
-        if(!session.passport){
-            return socket.disconnect()
+  const io = socket.listen(http);
+  io.use(function(socket,next){
+      app.session(socket.request,socket.request.res,next);
+      });
+  io.sockets.on('connection',function(socket){
+      var stream = null;
+      var tokens = require('./tokens').twitter;
+      var {session} = socket.request
+
+      if(!session.passport){
+      return socket.disconnect()
+      }
+      var profile = session.passport.user
+      var client = new twitter({
+consumer_key:tokens.consumerKey,
+consumer_secret:tokens.consumerSecret,
+access_token_key:profile.twitter_token,
+access_token_secret:profile.twitter_token_secret
+})
+      console.log('connected '+profile.displayName);
+      socket.on('search',(option)=>{
+        if(!option || !option.target){return}
+        if(stream){
+        stream.destroy()
         }
-        var profile = session.passport.user
-        var client = new twitter({
-            consumer_key:tokens.consumerKey,
-            consumer_secret:tokens.consumerSecret,
-            access_token_key:profile.twitter_token,
-            access_token_secret:profile.twitter_token_secret
-        })
-        socket.on('search',(option)=>{
-          if(!option || !option.target){return}
-          if(stream){
-              stream.destroy()
+        client.get('users/show',{screen_name:option.target},(err,data)=>{
+          if(err){
+            socket.emit('error',err);
+            return
           }
-          client.get('users/show',{screen_name:option.target},(err,data)=>{
-            if(err){return}
-            client.stream('statuses/filter',{follow:data.id},function(_stream){
-              stream = _stream;
-              stream.on('data',(data)=>{
-                  if(data.retweeted_status){return}
-                  socket.emit('tweet',data);
+          client.stream('statuses/filter',{follow:data.id},function(_stream){
+            stream = _stream;
+            stream.on('data',(data)=>{
+              if(data.retweeted_status){return}
+              socket.emit('tweet',data);
               })
             })
-        })
-      }) 
-    })
+            stream.on('error',(e)=>{socket.emit('error',e)})
+          })
+        }) 
+})
 }
